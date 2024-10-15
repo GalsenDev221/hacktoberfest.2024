e78354fb992678c3f5c9ebf0b02631bb63c29a7b
daoodaba975 <davinhoba9@outlook.fr>
2024-10-15 23:40:13 +0000

    [REFACTOR]: Mask agenda section 🧩

--- src/routes/_components/section-agenda.svelte
+++ src/routes/_components/section-agenda.svelte
@@ -1,13 +1,26 @@
 <script lang="ts">
 	import type { Schedule } from '$lib/types';
 
-	export let schedule: Schedule[];
+	// export let schedule: Schedule[];
 </script>
 
 <section class="container mx-auto px-4 py-10">
 	<h2 class="text-xl font-medium">Programme</h2>
 
 	<ul
+		class="mt-8 divide-y-[3px] divide-dashed divide-light/50 border-y-[3px] border-dashed border-light/50"
+	>
+		<li class="flex h-32 items-center sm:h-24">
+			<article class="grid grid-cols-1 items-center gap-2 sm:grid-cols-[200px_1fr]">
+				<h3 class="text-xl font-medium leading-none text-green">⌛</h3>
+				<div class="space-y-1 overflow-hidden">
+					<h4 class="truncate text-lg font-medium">Bientôt disponible !</h4>
+				</div>
+			</article>
+		</li>
+	</ul>
+
+	<!-- <ul
 		class="mt-8 divide-y-[3px] divide-dashed divide-light/50 border-y-[3px] border-dashed border-light/50"
 	>
 		{#each schedule as { title, time, speaker } (title)}
@@ -25,5 +38,5 @@
 				</article>
 			</li>
 		{/each}
-	</ul>
+	</ul> -->
 </section>
